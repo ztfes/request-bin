7cbcb1b0f643a10d08036e1cc2968049eba4bfe4
Jack Sebben <jacksebben11@hotmail.com>
2026-08-31 17:06:16 -0600

    MongoDB wired up

--- backend/db/mongo.py
+++ backend/db/mongo.py
@@ -6,5 +6,6 @@ from pymongo import MongoClient
 load_dotenv()
 
 MONGO_URL = os.getenv("MONGO_URL")
-
 client = MongoClient(MONGO_URL)
+db = client.get_default_database()          # -> "request_bin" from the URL
+requests_collection = db["captured_requests"]
